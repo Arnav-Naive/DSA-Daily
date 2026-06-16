08b925cb5906dc4da9af13f083d80c6a03429b5a
Arnav-Naive <arnavfating09@gmail.com>
2026-06-17 02:08:06 +0530

    Stack basic que

--- Stack/C_R_U_D.py
+++ Stack/C_R_U_D.py
@@ -1,1 +1,76 @@
-# Stack
+class Node:
+  def __init__(self, value):
+    self.data = value
+    self.next = None
+
+
+class Stack:
+  def __init__(self):
+    self.top = None
+
+
+  def isempty(self):
+    return self.top == None
+
+  # PUSH
+  def push(self, target):
+    new_node = Node(target)
+
+    new_node.next = self.top
+    self.top = new_node 
+
+  
+  # PEEK
+  def peek(self):
+    if self.isempty():
+      return "Stack Empty"
+    else:
+      return self.top.data
+
+  # POP
+  def pop(self):
+    if self.isempty():
+      return "Stack Empty"
+    else:
+      temp_data = self.top.data  # Value save karo
+      self.top = self.top.next   # Node delete karo
+      return temp_data           # Value return karo
+
+  # Traverse
+  def traverse(self):
+    temp = self.top
+
+    while temp != None:
+      print(temp.data)
+      temp = temp.next
+
+  # len (No necessaryly mantatory)
+  def __len__(self):      # just use --> len(s)
+    curr = self.top
+    count = 0
+    while curr != None:
+      count += 1
+      curr = curr.next
+    return count
+
+
+
+
+
+s = Stack()
+
+s.push(10)
+s.push(20)
+s.push(30)
+
+print("Top element is:", s.peek())
+
+print("Popped element is:", s.pop())
+
+print("Top element is:", s.peek())
+
+print("Length of stack:", len(s))
+
+print("Stack elements:")
+s.traverse()
+    
---
+++ Stack/stack-que.py
@@ -0,0 +1,53 @@
+#_______________________________________________________________________________
+# simple questions on Stack
+from C_R_U_D import Stack
+
+
+def reverse_string(text):
+
+  s = Stack()
+
+  for i in text:
+    s.push(i)
+
+  res = ""
+  while ( not s.isempty() ):  
+    res = res + s.pop()
+
+  return res
+
+# Good que
+# Text Editor
+
+def text_editor(text, pattern): # text = 'Hello' , pattern = "uurur"
+
+  undo = Stack()
+  redo = Stack()
+
+  for i in text:
+    undo.push(i)
+
+  for i in pattern:
+
+    if i == "u":
+      data = undo.pop()
+      redo.push(data)
+    else:
+      data = redo.pop()
+      undo.push(data)
+
+  #printing part
+  res = ""
+  while(not undo.isempty()):
+    res = undo.pop() + res
+  print(res)
+
+
+
+text_editor("Kolkata", "uuurr")
+
+#_______________________________________________________________________________
+
+# leetcode |  Balanced parentheses | Valid-Parentheses (Homework)
+
+

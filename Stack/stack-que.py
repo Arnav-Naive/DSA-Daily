4a890c751448ec92ce31dd59f72f53afcdede3f9
Arnav-Naive <arnavfating09@gmail.com>
2026-06-20 00:20:02 +0530

    need reunderstanding in the solution in FUTURE

--- Stack/stack-que.py
+++ Stack/stack-que.py
@@ -53,4 +53,46 @@ text_editor("Kolkata", "uuurr")
 
 #_______________________________________________________________________________
 # Celebrity problem 
+    ## https://www.geeksforgeeks.org/problems/the-celebrity-problem/1 
+    
+L = [
+  [0,0,1,0],  # 0 -> knows 2
+  [0,0,1,0],  # 1 -> knows 2
+  [1,0,0,1],  # 2 -> knows 0 and 3
+  [0,0,1,0]   # 3 -> knows 2
+]
 
+def celebrity(L):
+
+  s = Stack()
+
+  for i in range(len(L)):
+    s.push(i)
+
+  while s.size() >= 2:
+
+    a = s.pop()
+    b = s.pop()
+
+    if  L[a][b] == 0:
+      # b is not a celeb
+      s.push(a)
+      
+    else:
+      # a is not a celeb
+      s.push(b)
+
+  celeb = s.pop()
+
+  for i in range(len(L)):
+
+    if i == celeb: # if i is celebrity
+      continue
+
+    #check celeb is know by all OR celeb does not know anyone 
+    if  L[celeb][i] == 1 or L[i][celeb] == 0:
+      return -1
+
+  return celeb
+
+print(celebrity(L))
